e40c838bc8390afb7b550e26952558ca4a3ceb9a
Reyan <reyant2030@gmail.com>
2025-12-13 13:15:08 -0500

    velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions/Outtake.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Definitions;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Outtake
@@ -12,6 +13,7 @@ public class Outtake
         outtakeMotor = outtake.get(DcMotorEx.class, "Outtake");
         outtakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         outtakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        outtakeMotor.setDirection(DcMotor.Direction.REVERSE);
     }
 
     public void setOuttake(double velocity) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.OpModes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Definitions.DriveTrain;
